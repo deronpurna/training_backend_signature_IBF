b135b69c197dd85fb1731a9fd77fcce10260340a
deronpurna <deronpurna@gmail.com>
2022-11-09 06:54:20 +0000

    Deploy Challenge 1

--- run.py
+++ run.py
@@ -1,13 +1,47 @@
 from flask import Flask
+from flask import Response
+from flask_ngrok import run_with_ngrok #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku
+import json
 
-#buat variabel utama buat procfile
+f = open('/content/training_backend_signature_IBF/training_backend_signature_IBF/about.json')
+f_cb = open('/content/training_backend_signature_IBF/training_backend_signature_IBF/dissolved_filter_clipped_CB_me_75.geojson')
+f_rh = open('/content/training_backend_signature_IBF/training_backend_signature_IBF/dissoloved_filter_clipped_RH_700_me_70.geojson')
+f_tp = open('/content/training_backend_signature_IBF/training_backend_signature_IBF/dissolved_filter_clipped_TP.geojson')
+
+geodata = json.load(f) #ini udah jadi dictionary
+geodata_cb = json.load(f_cb)
+geodata_rh = json.load(f_rh)
+geodata_tp = json.load(f_tp)
+
 pelatihan_ibf_app = Flask(__name__)
-
+run_with_ngrok(pelatihan_ibf_app) #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku  
 
 @pelatihan_ibf_app.route('/')
-def hello_world():
-    return 'Hello World!'
+def send_json_data():
+    return Response(response=json.dumps(geodata), #ngubah dari dict ke json pakai respone dan .dumps
+                    status=200, # untuk OK, 404 untuk NOT FOUND
+                    mimetype="application/json")
+    
+@pelatihan_ibf_app.route('/cb')
+def send_json_data_cb():
+    geodataspec = geodata_cb
+    return Response(response=json.dumps(geodataspec), 
+                    status=200,
+                    mimetype="application/json")
+    
+@pelatihan_ibf_app.route('/rh')
+def send_json_data_rh():
+    geodataspec = geodata_rh
+    return Response(response=json.dumps(geodataspec),
+                    status=200,
+                    mimetype="application/json")
 
+@pelatihan_ibf_app.route('/tp')
+def send_json_data_tp():
+    geodataspec = geodata_tp
+    return Response(response=json.dumps(geodataspec),
+                    status=200,
+                    mimetype="application/json")
 
 if __name__ == '__main__':
     pelatihan_ibf_app.run()
