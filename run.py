f68d4e1c9e84559c93dd1cd5f12c782a0c005938
deronpurna <deronpurna@gmail.com>
2022-11-09 09:41:04 +0000

    Deploy Challenge 2

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from flask import Response
 #from flask_ngrok import run_with_ngrok #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku
 import json
 
+from jinja2.environment import internalcode
+
 f = open('./about.json')
 f_cb = open('./dissolved_filter_clipped_CB_me_75.geojson')
 f_rh = open('./dissoloved_filter_clipped_RH_700_me_70.geojson')
@@ -12,6 +14,7 @@ geodata = json.load(f) #ini udah jadi dictionary
 geodata_cb = json.load(f_cb)
 geodata_rh = json.load(f_rh)
 geodata_tp = json.load(f_tp)
+
 
 pelatihan_ibf_app = Flask(__name__)
 #run_with_ngrok(pelatihan_ibf_app) #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku  
@@ -42,6 +45,59 @@ def send_json_data_tp():
     return Response(response=json.dumps(geodataspec),
                     status=200,
                     mimetype="application/json")
+
+
+@pelatihan_ibf_app.route('/equals')
+def send_json_data_equals():
+    param = request.args.get("var")
+    if param == "cb":
+      geodata = geodata_cb
+    elif param == "rh":
+      geodata = geodata_rh
+    else:
+      geodata = geodata_tp
+    value = request.args.get('value')
+
+    dataquery = [p for p in geodata["features"] if p["properties"]["value"] == int(value)] #perhatikan jenis variable
+
+    return Response(response=json.dumps(dataquery),
+                    status=200,
+                    mimetype="application/json")
+
+@pelatihan_ibf_app.route('/morethan')
+def send_json_data_morethan():
+    param = request.args.get("var")
+    if param == "cb":
+      geodata = geodata_cb
+    elif param == "rh":
+      geodata = geodata_rh
+    else:
+      geodata = geodata_tp
+    value = request.args.get('value')
+
+    dataquery = [p for p in geodata["features"] if p["properties"]["value"] > int(value)] #perhatikan jenis variable
+
+    return Response(response=json.dumps(dataquery),
+                    status=200,
+                    mimetype="application/json")
+
+@pelatihan_ibf_app.route('/lessthan')
+def send_json_data_lessthan():
+    param = request.args.get("var")
+    if param == "cb":
+      geodata = geodata_cb
+    elif param == "rh":
+      geodata = geodata_rh
+    else:
+      geodata = geodata_tp
+    value = request.args.get('value')
+
+    dataquery = [p for p in geodata["features"] if p["properties"]["value"] < int(value)] #perhatikan jenis variable
+
+    return Response(response=json.dumps(dataquery),
+                    status=200,
+                    mimetype="application/json")
+
 
 if __name__ == '__main__':
     pelatihan_ibf_app.run()
